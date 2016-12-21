eaaafe61b9016f859a5c5773f0d8b175b8ad8982
Kensuke Kosaka <k@trileg.net>
2016-12-21 19:32:38 +0900

    一定の確率で元データの値域の乱数でデータを上書きする機能を追加

--- AddNoise.cpp
+++ AddNoise.cpp
@@ -30,3 +30,31 @@ vector<vector<double>> add_noise(const vector<vector<double>> &input, const floa
 
   return result;
 }
+
+/**
+ * データごとに0.0以上1.0未満の乱数を生成し，rate未満であればそのデータをデータセットの最小値以上最大値未満の乱数にする
+ * @param input ノイズをのせるデータ
+ * @param rate ノイズをのせる確率
+ * @return ノイズをのせたデータ
+ */
+vector<vector<double>> add_random_noise(const vector<vector<double>> &input, const float rate) {
+  random_device rnd;
+  mt19937 mt;
+  mt.seed(rnd());
+  uniform_real_distribution<double> rnd_zero_one(0.0, 1.0);
+
+  vector<vector<double>> result(input);
+
+  for (unsigned long i = 0, i_s = result.size(); i < i_s; ++i) {
+    double min = *std::min_element(result[i].begin(), result[i].end());
+    double max = *std::max_element(result[i].begin(), result[i].end());
+    uniform_real_distribution<double> rnd_val(min, max);
+    for (unsigned long j = 0, j_s = result[i].size(); j < j_s; ++j) {
+      if (rnd_zero_one(mt) < rate) {
+        result[i][j] = rnd_val(mt);
+      }
+    }
+  }
+
+  return result;
+}
--- AddNoise.h
+++ AddNoise.h
@@ -7,5 +7,6 @@
 using std::vector;
 
 vector<vector<double>> add_noise(const vector<vector<double>> &input, const float rate);
+vector<vector<double>> add_random_noise(const vector<vector<double>> &input, const float rate);
 
 #endif //NN_CUDA_ADDNOISE_H
